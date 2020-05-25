5075108013c0d1fdebee34f365d68ba59dd0017d
Bhuvaneswari Babu <bhuvanamouttou@gmail.com>
2020-05-25 13:52:23 +0200

    Updates

---
+++ precache-manifest.05624f75c92e91e962492b06c3205cdd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372febc7ff821b522ae1fcefc389c6e1",
+    "url": "/react-robo-friends/index.html"
+  },
+  {
+    "revision": "ed88ff4cc0aa9677e0a1",
+    "url": "/react-robo-friends/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1323c2eb835351db7d2c",
+    "url": "/react-robo-friends/static/css/main.7197a695.chunk.css"
+  },
+  {
+    "revision": "ed88ff4cc0aa9677e0a1",
+    "url": "/react-robo-friends/static/js/2.a9b0da29.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-robo-friends/static/js/2.a9b0da29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1323c2eb835351db7d2c",
+    "url": "/react-robo-friends/static/js/main.037f708d.chunk.js"
+  },
+  {
+    "revision": "2d42226de32ae77eab5f",
+    "url": "/react-robo-friends/static/js/runtime-main.2e32e749.js"
+  },
+  {
+    "revision": "3f5c9111609279eb69be9e1a8a10e82b",
+    "url": "/react-robo-friends/static/media/SEGA-LOGO-FONT.ttf.3f5c9111.woff"
+  },
+  {
+    "revision": "57ad6cb1828e85f5f029d166ac0361b8",
+    "url": "/react-robo-friends/static/media/SEGA-LOGO-FONT.ttf.57ad6cb1.eot"
+  },
+  {
+    "revision": "d7d67c411a56f01384056ff8a85eb4c6",
+    "url": "/react-robo-friends/static/media/SEGA-LOGO-FONT.ttf.d7d67c41.svg"
+  }
+]);
